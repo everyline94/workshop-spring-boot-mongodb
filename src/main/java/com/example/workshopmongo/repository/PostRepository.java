515cbcaf78fd699e7c66dfbbed26af14d467879a
bruno larocca <brunolujanlarocca@gmail.com>
2023-08-10 10:38:13 -0300

    Consulta simples com query methods

--- src/main/java/com/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package com.example.workshopmongo.repository;
 
 import com.example.workshopmongo.domain.Post;
-import com.example.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
 }
